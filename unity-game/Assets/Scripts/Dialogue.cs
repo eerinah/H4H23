f1e7799017ffbf8588d756f873cedb14a2f106a9
Gwendolyn Patwardhan <52841252+gwynin@users.noreply.github.com>
2023-02-19 07:20:03 -0800

    aesthetic tweaks

--- unity-game/Assets/Scripts/Dialogue.cs
+++ unity-game/Assets/Scripts/Dialogue.cs
@@ -22,7 +22,7 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
+        if(Input.GetMouseButtonDown(0))
         {
             if(textComponent.text == lines[index])
             {
